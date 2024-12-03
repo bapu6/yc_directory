9a3998446cca58aac2012b126352932d84951111
Bapu <bapup@koinearth.com>
2024-12-03 23:26:41 +0530

    add optional chaning

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -14,10 +14,11 @@ import View from "@/components/View";
 import StartupCard, { StartUpTypeCard } from "@/components/StartupCard";
 
 export const experimental_ppr = true;
+
 const md = markdown();
 
 const StartUp = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const id = (await params).id;
+  const id = (await params)?.id;
 
   const [post, { select: softwarePosts }] = await Promise.all([
     await client.fetch(STARTUPS_BY_ID, { id }),
@@ -32,12 +33,12 @@ const StartUp = async ({ params }: { params: Promise<{ id: string }> }) => {
     <>
       <section className="pink_container">
         <p className="tag">{formatDate(post?._createdAt)}</p>
-        <h1 className="heading">{post.title}</h1>
-        <p className="sub-heading !max-w-5xl">{post.description}</p>
+        <h1 className="heading">{post?.title}</h1>
+        <p className="sub-heading !max-w-5xl">{post?.description}</p>
       </section>
       <section className="section_container">
         <Image
-          src={post.image || null}
+          src={post?.image || null}
           alt="post-image"
           width={0}
           height={0}
@@ -47,24 +48,24 @@ const StartUp = async ({ params }: { params: Promise<{ id: string }> }) => {
         <div className="space-y-5 mt-10 max-w-4xl mx-auto">
           <div className="flex-between gap-5">
             <Link
-              href={`/user/${post.author?._id}`}
+              href={`/user/${post?.author?._id}`}
               className="flex gap-2 items-center mb-3"
             >
               <Image
-                src={post.author?.image || null}
+                src={post?.author?.image || null}
                 alt="author-image"
                 width={64}
                 height={64}
                 className="rounded-full drop-shadow-lg"
               />
               <div>
-                <p className="text-20-medium">{post.author.name}</p>
+                <p className="text-20-medium">{post?.author?.name}</p>
                 <p className="text-16-medium !text-black-300">
                   @{post?.author?.username}
                 </p>
               </div>
             </Link>
-            <p className="category-tag">{post.category}</p>
+            <p className="category-tag">{post?.category}</p>
           </div>
           <h3 className="text-30-bold">Pitch Details</h3>
           {parsedContent ? (
@@ -91,7 +92,6 @@ const StartUp = async ({ params }: { params: Promise<{ id: string }> }) => {
           </div>
         )}
 
-        {/* Todo:Editor Selected startup */}
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
         </Suspense>
